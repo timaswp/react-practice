3bd79c09d1c164ade3cedb24c6d74744bc142124
Teymur <melikovteymur@mail.ru>
2024-08-28 23:48:26 +0400

    posts important and like interactivity

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,8 +7,21 @@ import PostList from "../post-list";
 import PostAddForm from "../post-add-form/post-add-form";
 
 import "./app.css";
+import "../app-header/app-header.css";
+import "../post-add-form/post-add-form.css";
+import "../post-list/post-list.css";
+import "../post-list-item/post-list-item.css";
+import "../post-status-filter/post-status-filter.css";
+import "../search-panel/search-panel.css";
 
 const App = () => {
+
+    const data = [
+        {label: 'Going to learn React', important: true, id: 'kdhi'},
+        {label: 'That is so good', important: false, id: 'pxie'},
+        {label: 'I need a break...', important: false, id: 'qwkc'}
+    ];
+
     return (
         <div className="app">
             <AppHeader/>
@@ -16,7 +29,7 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data}/>
             <PostAddForm/>
         </div>
     )
--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,30 +1,67 @@
-import React from "react";
+import React, {Component} from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
-const PostListItem = () => {
-    return (
-        <li className="app-list-item d-flex justify-content-between">
-            <span className="app-list-item-label">
-                Hello World
-            </span>
-            <div className="d-flex justify-content-center align-items-center">
-                <button
+export default class PostListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            important: false,
+            liked: false
+        };
+        this.onImportant = this.onImportant.bind(this);
+        this.onLiked = this.onLiked.bind(this);
+    }
+
+    onImportant() {
+        this.setState(({important}) => ({
+            important: !important
+        }));
+    }
+
+    onLiked() {
+        this.setState(({liked}) => ({
+            liked: !liked
+        }));
+    }
+
+    render() {
+
+        const {label} = this.props;
+        const {important, liked} = this.state;
+        let classNames = "app-list-item d-flex justify-content-between";
+        if (important) {
+            classNames += ' important';
+        }
+
+        if (liked) {
+            classNames += ' like';
+        }
+
+        return (
+            <div className={classNames}>
+                <span
+                className="app-list-item-label"
+                onClick={this.onLiked}>
+                    {label}
+                </span>
+                <div className="d-flex justify-content-center align-items-center">
+                    <button
                     type="button"
-                    className="btn-star btn-sm">
-                    <FontAwesomeIcon icon={faStar} />
-                </button>
-                <button
+                    className="btn-star btn-sm"
+                    onClick={this.onImportant}>
+                        <FontAwesomeIcon icon={faStar} />
+                    </button>
+                    <button
                     type="button"
                     className="btn-trash btn-sm">
-                    <FontAwesomeIcon icon={faTrashCan} />
-                </button>
-                <FontAwesomeIcon icon={faHeart} />
+                        <FontAwesomeIcon icon={faTrashCan} />
+                    </button>
+                    <FontAwesomeIcon icon={faHeart} />
+                </div>
             </div>
-        </li>
-    )
+        )
+    }
 }
-
-export default PostListItem;
